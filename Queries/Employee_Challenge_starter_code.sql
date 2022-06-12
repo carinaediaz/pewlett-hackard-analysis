8353f26ca6d8b9ededf7137d4c9359e0d4046446
carinaediaz <carinaediaz96@gmail.com>
2022-06-12 12:49:00 -0500

    adding additional queries for deliverable 3

--- Queries/Employee_Challenge_starter_code.sql
+++ Queries/Employee_Challenge_starter_code.sql
@@ -48,3 +48,21 @@ LEFT JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE de.to_date = '9999-01-01' AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY emp_no;
+
+
+-- Create mentorship_elegibility table by expanding DOB filter
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date, 
+    de.from_date, 
+    de.to_date, 
+    t.title
+INTO mentorship_eligibility_expanded
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01' AND (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+ORDER BY emp_no;
